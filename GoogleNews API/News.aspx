d7a561d3ddde85c0e243c20d949b3b9403ad6610
Cory R Siebler <cory.siebler@gmail.com>
2015-04-03 13:33:55 -0700

    Added Project

---
+++ GoogleNews API/News.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="News.aspx.cs" Inherits="GoogleNews_API.News" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Google News TryIt</title>
+    <script type="text/javascript" src="Scripts/jquery-2.1.0.min.js"></script>
+    <script type="text/javascript">
+        // Function for GetNews using Ajax Post Method
+        function GetNews() {
+            // Create Ajax Post Method
+            $.ajax({
+                type: "POST", // Ajax Mehod
+                url: "News.aspx/GetNewsArticles",  // Page URL / Method name
+                data: "{'topic':'" + document.getElementById("input").value + "'}", // Pass Parameters
+                contentType: "application/json;charset=utf-8",
+                dataType: "json",
+                success: function (data) { // Function call on success
+                    $("#DivNews").empty(); // Set Div Empty
+
+                    // Set For loop for binding Div Row wise
+                    for (var i = 0; i < data.d.length; i++) {
+                        // Design Div Dynamically using append
+                        $("#DivNews").append("<a target=\"_blank\" href=\"" + data.d[i].url + "\">" + data.d[i].title + "</a><br />");
+                    }
+                },
+                error: function (result) {
+                    // Function call on failure or error
+                    alert(result.d);
+                }
+            });
+
+            return false;
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table>
+            <tr>
+                <td align="center" class="style1">
+                    <h3>Google News TryIt</h3>
+                </td>
+            </tr>
+            <tr>
+                <td>Method Signature: GetNewsArticles(string):List</td>
+            </tr>
+            <tr>
+                <td>String Value: <asp:TextBox runat="server" ID="input" CssClass="textbox" /></td>
+                <td><asp:Button id="Button1" Text="Submit" OnClientClick="return GetNews()" runat="server"/></td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <div id="DivNews"></div>
+    </form>
+</body>
+</html>
---
+++ GoogleNews API/News.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Data;
+
+namespace GoogleNews_API
+{
+    public partial class News : System.Web.UI.Page
+    {
+        /// <summary>
+        /// Page Load Method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="topic"></param>
+        /// <returns></returns>
+        [WebMethod]
+        public static NewsArticle[] GetNewsArticles(string topic)
+        {
+            // Initialize the List of News Articles
+            List<NewsArticle> articles = new List<NewsArticle>();
+
+            // Initialize the Google News RSS Feed request
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://news.google.com/news?q=" + topic + "&output=rss");
+
+            // Specify GET Method Request
+            request.Method = "GET";
+
+            // Perform the Request
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Check the Response State Code for Success
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                // Check the Character Set of the Response
+                if (response.CharacterSet == "")
+                {
+                    readStream = new StreamReader(receiveStream);
+                }
+                else
+                {
+                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                // Convert the Stream in a JSON string
+                string data = readStream.ReadToEnd();
+
+                // Initialize a DataSet to store the parsed information
+                DataSet ds = new DataSet();
+                StringReader reader = new StringReader(data);
+                ds.ReadXml(reader);
+                DataTable articleTable = new DataTable();
+
+                // Make sure the response is not empty
+                if (ds.Tables.Count > 0)
+                {
+                    articleTable = ds.Tables["item"];
+
+                    // Loop through each RSS element and store it as a NewsArticle
+                    foreach (DataRow row in articleTable.Rows)
+                    {
+                        NewsArticle article = new NewsArticle();
+                        article.id = row["item_id"].ToString(); 
+                        article.title = row["title"].ToString();
+                        article.url = row["link"].ToString();
+                        article.date = row["pubDate"].ToString();
+                        article.description = row["description"].ToString();
+                        articles.Add(article);
+                    }
+                }
+            }
+
+            return articles.ToArray();
+        }
+
+        public class NewsArticle
+        {
+            public string title { get; set; }
+            public string url { get; set; }
+            public string date { get; set; }
+            public string id { get; set; }
+            public string description { get; set; }
+        }
+    }
+}
